e989010478548946ce175a1d728b74513c2b5226
springcalf <springcalf@gmail.com>
2015-07-18 20:21:36 +0800

    Initial Commit

---
+++ Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by WuSongHua on 15-7-18.
+//  Copyright (c) 2015年 WuSongHua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by WuSongHua on 15-7-18.
+//  Copyright (c) 2015年 WuSongHua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
